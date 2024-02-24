d1ac4798077ff5e1789befdb200bca35244bb109
cruelwxrld <146163829+cruelwxrld@users.noreply.github.com>
2024-02-24 14:21:13 +0300

    Delete src/index_task2.js

--- src/index_task2.js
+++
@@ -1,8 +0,0 @@
-function celsiusToFahrenheit(celsius) {
-  var fahrenheit = (celsius * 9/5) + 32;
-  return fahrenheit;
-}
-
-var celsius = parseFloat(prompt("Введите температуру в градусах Цельсия:"));
-
-console.log("Температура в градусах Фаренгейта:", celsiusToFahrenheit(celsius));
