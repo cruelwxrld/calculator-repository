79746bf37967a3b8e330825fb2f3931f8d6a0e3e
cruelwxrld <146163829+cruelwxrld@users.noreply.github.com>
2024-02-24 14:21:18 +0300

    Delete src/index_task1.js

--- src/index_task1.js
+++
@@ -1,8 +0,0 @@
-function incomeTaxCalculator(income) {
-  var tax = income * 0.15;
-  return tax;
-}
-
-var income = parseFloat(prompt("Введите сумму дохода:"));
-
-console.log("Сумма налога на доходы:", incomeTaxCalculator(income));
